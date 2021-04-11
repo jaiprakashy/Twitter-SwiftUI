93c27ae5935720db84711b874e7b30f9ef228a90
Jaiprakash Yadav <yadavjaiprakash20@gmail.com>
2021-04-11 11:28:54 +0530

    Initial Commit

---
+++ Twitter/TwitterApp.swift
@@ -0,0 +1,17 @@
+//
+//  TwitterApp.swift
+//  Twitter
+//
+//  Created by Jaiprakash Yadav on 11/04/21.
+//
+
+import SwiftUI
+
+@main
+struct TwitterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
